28768dfe00dd57b0b8882931ee373c202490fc7c
Cristian <cristian.lazar.uk@gmail.com>
2021-01-05 16:54:21 +0000

    FizzBuzz test

--- scripts/drink.js
+++ scripts/drink.js
@@ -18,3 +18,18 @@ whatCanIDrink = function (age) {
         return "Sorry. I can’t tell what drink because that age is incorrect!";
     }
 };
+
+fizzBuzz = function(number) {
+	if (number % 3 === 0 && number % 5 === 0) {
+		return "FizzBuzz";
+	}
+	else if (number % 3 === 0) {
+		return "Fizz";
+	}
+	else if (number % 5 === 0) {
+		return "Buzz";
+	}	
+	else {
+		return number
+	}
+};
